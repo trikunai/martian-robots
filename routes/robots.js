0e966577692e129eb2adca5e4aab6eb349d9df9e
trikunai <triku25@gmail.com>
2021-02-07 10:37:19 +0100

    swagger jsdoc document

--- routes/robots.js
+++ routes/robots.js
@@ -3,16 +3,71 @@ const Robots = require('./../controllers/robots');
 module.exports = (router) => {
 /**
      * @swagger
-	 * tags:
-	 *  - name: Robots
-	 *  description: Calls related to Martian Robots
-	 * definitions:
-	 *   Input:
-	 * 	   properties:
-	 * 		  instructions:
-	 * 			type: "string"
-     *          example: "5 3\n1 1 E\nRFRFFFFFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL"
-*
+	 *  definitions:
+	 *     gridStat:
+	 *       type: object
+	 *       description: "Explored grid accumulated stats (robots movements at grid)"
+	 *       properties:
+	 *         id:
+	 *           type: integer
+	 *           example: 1
+	 *           description: "db autoincremental id"
+	 *         totalForwardMovements:
+	 *           type: integer
+	 *           example: 2
+	 *           description: "Sum of all forward movements done by all robots in this grid (accumulated)"
+	 *         totalRightMovements:
+	 *           type: integer
+	 *           example: 3
+	 *           description: "Sum of all right rotate movements done by all robots in this grid (accumulated)"
+	 *         totalLeftMovements:
+	 *           type: integer
+	 *           example: 4
+	 *           description: "Sum of all left rotate movements done by all robots in this grid (accumulated)"
+	 *         totalLostRobots:
+	 *           type: integer
+	 *           example: 5
+	 *           description: "Sum of all lost robots in this grid (accumulated)"
+	 *         createdAt:
+	 *           type: string
+	 *           example: "2021-02-06T16:37:53.416Z"
+	 *           description: "Grid stat creation datetime string"
+	 *         updatedAt:
+	 *           type: string
+	 *           example: "2021-02-06T16:37:53.416Z"
+	 *           description: "Grid stat last update datetime string"
+	 *     robotStats:
+	 *       type: object
+     *       properties:
+     *         id:
+	 *           type: integer
+     *           example: 1
+     *           description: "new deployed robot stats id"
+     *         idRobot:
+     *           type: integer
+     *           example: 1
+     *           description: "new deployed robot id"
+     *         r:
+     *           type: integer
+     *           example: 2
+     *           description: "Sum of all right rotate movements done by this robot until its final coordinates"
+     *         l:
+     *           type: integer
+     *           example: 6
+     *           description: "Sum of all left rotate movements done by this robot until its final coordinates"
+     *         f:
+     *           type: integer
+     *           example: 3
+     *           description: "Sum of all forward movements done by this robot until its final coordinates"
+	 *         createdAt:
+	 *           type: string
+	 *           example: "2021-02-06T16:37:55.416Z"
+	 *           description: "Grid stat creation datetime string"
+	 *         updatedAt:
+	 *           type: string
+	 *           example: "2021-02-06T16:37:55.416Z"
+	 *           description: "Grid stat last update datetime string"
+     */
 /**
      * @swagger
      * /robots:
@@ -31,10 +86,56 @@ module.exports = (router) => {
 	 *              properties:
 	 *                instructions:
 	 *                  type: string
-	 *                  example: "gsrggrred"
+	 *                  example: "5 3\n1 1 E\nRFRFFFFFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL"
+	 *                  description: "grid, robots, movements instructions lines input"
      *     responses:
      *       200:
      *         description: "An array of apps"
+	 *         content:
+     *           application/json:
+     *             schema:
+	 *               type: object
+	 *               properties:
+	 *                 output:
+	 *                   type: string
+	 *                   example: "1 1 E\n3 2 N\n2 3 S"
+	 *                   description: "Procesed output of the given input"
+	 *                 robotStats:
+	 *                   type: "array"
+	 *                   items:
+	 *                     $ref: '#/definitions/robotStats'
+	 *                 gridStat:
+	 *                   type: object
+	 *                   description: "Explored grid accumulated stats (robots movements at grid)"
+	 *                   properties:
+	 *                     id:
+	 *                       type: integer
+	 *                       example: 1
+	 *                       description: "db autoincremental id"
+	 *                     totalForwardMovements:
+	 *                       type: integer
+	 *                       example: 2
+	 *                       description: "Sum of all forward movements done by all robots in this grid (accumulated)"
+	 *                     totalRightMovements:
+	 *                       type: integer
+	 *                       example: 3
+	 *                       description: "Sum of all right rotate movements done by all robots in this grid (accumulated)"
+	 *                     totalLeftMovements:
+	 *                       type: integer
+	 *                       example: 4
+	 *                       description: "Sum of all left rotate movements done by all robots in this grid (accumulated)"
+	 *                     totalLostRobots:
+	 *                       type: integer
+	 *                       example: 5
+	 *                       description: "Sum of all lost robots in this grid (accumulated)"
+	 *                     createdAt:
+	 *                       type: string
+	 *                       example: "2021-02-06T16:37:53.416Z"
+	 *                       description: "Grid stat creation datetime string"
+	 *                     updatedAt:
+	 *                       type: string
+	 *                       example: "2021-02-06T16:37:53.416Z"
+	 *                       description: "Grid stat last update datetime string"
      *       500:
      *         description: "Error response"
      *     operationId: "robotInstructions"
